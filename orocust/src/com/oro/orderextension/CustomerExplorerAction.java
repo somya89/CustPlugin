992469e0c926ca146bfe6bbf477e6b1cf1c3187d
somya <somya89@gmail.com>
2015-10-05 23:07:15 +0530

    few find bugs cleared

--- orocust/src/com/oro/orderextension/CustomerExplorerAction.java
+++ orocust/src/com/oro/orderextension/CustomerExplorerAction.java
@@ -1,48 +1,52 @@
-/*    */ package com.oro.orderextension;
-/*    */ 
-/*    */ import com.floreantpos.bo.ui.BackOfficeWindow;
-/*    */ import com.floreantpos.customer.CustomerExplorer;
-/*    */ import com.floreantpos.util.POSUtil;
-/*    */ import java.awt.event.ActionEvent;
-/*    */ import javax.swing.AbstractAction;
-/*    */ import javax.swing.Icon;
-/*    */ import javax.swing.JTabbedPane;
-/*    */ 
-/*    */ public class CustomerExplorerAction
-/*    */   extends AbstractAction
-/*    */ {
-/*    */   public CustomerExplorerAction()
-/*    */   {
-/* 16 */     super("Customer");
-/*    */   }
-/*    */   
-/*    */   public CustomerExplorerAction(String name) {
-/* 20 */     super(name);
-/*    */   }
-/*    */   
-/*    */   public CustomerExplorerAction(String name, Icon icon) {
-/* 24 */     super(name, icon);
-/*    */   }
-/*    */   
-/*    */   public void actionPerformed(ActionEvent e) {
-/* 28 */     BackOfficeWindow backOfficeWindow = POSUtil.getBackOfficeWindow();
-/*    */     
-/* 30 */     CustomerExplorer explorer = null;
-/* 31 */     JTabbedPane tabbedPane = backOfficeWindow.getTabbedPane();
-/* 32 */     int index = tabbedPane.indexOfTab("Customer");
-/* 33 */     if (index == -1) {
-/* 34 */       explorer = new CustomerExplorer();
-/* 35 */       tabbedPane.addTab("Customer", explorer);
-/*    */     }
-/*    */     else {
-/* 38 */       explorer = (CustomerExplorer)tabbedPane.getComponentAt(index);
-/*    */     }
-/* 40 */     tabbedPane.setSelectedComponent(explorer);
-/*    */   }
-/*    */ }
+package com.oro.orderextension;
 
+import java.awt.event.ActionEvent;
 
-/* Location:              C:\Users\SOMYA\Desktop\divya\orocust-0.7.jar!\com\oro\orderextension\CustomerExplorerAction.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       0.7.1
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+import javax.swing.JTabbedPane;
+
+import com.floreantpos.bo.ui.BackOfficeWindow;
+import com.floreantpos.customer.CustomerExplorer;
+
+public class CustomerExplorerAction extends AbstractAction {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7818075409452485741L;
+
+	public CustomerExplorerAction() {
+		super("Customer");
+	}
+
+	public CustomerExplorerAction(String name) {
+		super(name);
+	}
+
+	public CustomerExplorerAction(String name, Icon icon) {
+		super(name, icon);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		BackOfficeWindow backOfficeWindow = BackOfficeWindow.getInstance();
+
+		CustomerExplorer explorer = null;
+		JTabbedPane tabbedPane = backOfficeWindow.getTabbedPane();
+		int index = tabbedPane.indexOfTab("Customer");
+		if (index == -1) {
+			explorer = new CustomerExplorer();
+			tabbedPane.addTab("Customer", explorer);
+		} else {
+			explorer = (CustomerExplorer) tabbedPane.getComponentAt(index);
+		}
+		tabbedPane.setSelectedComponent(explorer);
+	}
+
+}
+
+/*
+ * Location:
+ * C:\Users\SOMYA\Desktop\divya\orocust-0.7.jar!\com\oro\orderextension
+ * \CustomerExplorerAction.class Java compiler version: 5 (49.0) JD-Core
+ * Version: 0.7.1
  */
--- orocust/src/com/oro/orderextension/OrderWithCustomerService.java
+++ orocust/src/com/oro/orderextension/OrderWithCustomerService.java
@@ -1,148 +1,131 @@
-/*     */ package com.oro.orderextension;
-/*     */ 
-/*     */ import com.floreantpos.main.Application;
-/*     */ import com.floreantpos.model.Customer;
-/*     */ import com.floreantpos.model.OrderType;
-/*     */ import com.floreantpos.model.Ticket;
-/*     */ import com.floreantpos.model.User;
-/*     */ import com.floreantpos.model.dao.TicketDAO;
-/*     */ import com.floreantpos.model.dao.UserDAO;
-/*     */ import com.floreantpos.ui.dialog.POSMessageDialog;
-/*     */ import com.floreantpos.ui.views.order.DefaultOrderServiceExtension;
-/*     */ import com.floreantpos.ui.views.order.OrderController;
-/*     */ import com.floreantpos.ui.views.order.OrderView;
-/*     */ import com.floreantpos.ui.views.order.RootView;
-/*     */ import com.floreantpos.util.POSUtil;
-/*     */ import com.floreantpos.util.TicketAlreadyExistsException;
-/*     */ import java.util.Calendar;
-/*     */ import java.util.Date;
-/*     */ import java.util.List;
-/*     */ import javax.swing.JMenu;
-/*     */ import javax.swing.JOptionPane;
-/*     */ import net.xeoh.plugins.base.annotations.PluginImplementation;
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ @PluginImplementation
-/*     */ public class OrderWithCustomerService
-/*     */   extends DefaultOrderServiceExtension
-/*     */ {
-/*     */   public String getName()
-/*     */   {
-/*  33 */     return "Order+Customer.";
-/*     */   }
-/*     */   
-/*     */   public String getDescription() {
-/*  37 */     return "This extension enables storing customer information with ticekt";
-/*     */   }
-/*     */   
-/*     */ 
-/*     */   public void init() {}
-/*     */   
-/*     */   public void createNewTicket(OrderType ticketType)
-/*     */     throws TicketAlreadyExistsException
-/*     */   {
-/*  46 */     DeliverySelectionDialog dialog = new DeliverySelectionDialog(Application.getPosWindow(), ticketType);
-/*  47 */     dialog.setSize(800, 650);
-/*  48 */     dialog.setLocationRelativeTo(Application.getPosWindow());
-/*  49 */     dialog.setVisible(true);
-/*     */     
-/*  51 */     if (!dialog.isCanceled()) {
-/*  52 */       Customer customer = dialog.getCustomer();
-/*  53 */       Date deliveryDate = dialog.getDeliveryDate();
-/*  54 */       String shippingAddress = dialog.getDeliveryAddress();
-/*     */       
-/*  56 */       Ticket ticket = new Ticket();
-/*  57 */       ticket.setPriceIncludesTax(Application.getInstance().isPriceIncludesTax());
-/*  58 */       ticket.setType(ticketType);
-/*  59 */       ticket.setTerminal(Application.getInstance().getTerminal());
-/*  60 */       ticket.setOwner(Application.getCurrentUser());
-/*  61 */       ticket.setShift(Application.getInstance().getCurrentShift());
-/*     */       
-/*  63 */       ticket.setCustomer(customer);
-/*     */       
-/*  65 */       if (dialog.willCustomerPickup()) {
-/*  66 */         ticket.setCustomerWillPickup(Boolean.valueOf(true));
-/*     */       }
-/*     */       else {
-/*  69 */         ticket.setDeliveryDate(deliveryDate);
-/*  70 */         ticket.setDeliveryAddress(shippingAddress);
-/*  71 */         ticket.setExtraDeliveryInfo(dialog.getExtraDeliveryInfo());
-/*  72 */         ticket.setCustomerWillPickup(Boolean.valueOf(false));
-/*     */       }
-/*     */       
-/*  75 */       Calendar currentTime = Calendar.getInstance();
-/*  76 */       ticket.setCreateDate(currentTime.getTime());
-/*  77 */       ticket.setCreationHour(Integer.valueOf(currentTime.get(11)));
-/*     */       
-/*  79 */       OrderView.getInstance().setCurrentTicket(ticket);
-/*  80 */       RootView.getInstance().showView("ORDER_VIEW");
-/*     */     }
-/*     */   }
-/*     */   
-/*     */ 
-/*     */ 
-/*     */   public void setCustomerToTicket(int ticketId) {}
-/*     */   
-/*     */ 
-/*     */ 
-/*     */   public void setDeliveryDate(int ticketId) {}
-/*     */   
-/*     */ 
-/*     */   public void assignDriver(int ticketId)
-/*     */   {
-/*  95 */     List<User> drivers = UserDAO.getInstance().findDrivers();
-/*     */     
-/*  97 */     if ((drivers == null) || (drivers.size() == 0)) {
-/*  98 */       POSMessageDialog.showError(Application.getPosWindow(), "No driver found to assign");
-/*  99 */       return;
-/*     */     }
-/*     */     
-/* 102 */     Ticket ticket = TicketDAO.getInstance().get(Integer.valueOf(ticketId));
-/*     */     
-/* 104 */     AssignDriverDialog dialog = new AssignDriverDialog(Application.getPosWindow());
-/* 105 */     dialog.setData(ticket, drivers);
-/* 106 */     dialog.setSize(550, 450);
-/* 107 */     dialog.setLocationRelativeTo(Application.getPosWindow());
-/* 108 */     dialog.setVisible(true);
-/*     */   }
-/*     */   
-/*     */   public boolean finishOrder(int ticketId)
-/*     */   {
-/* 113 */     Ticket ticket = TicketDAO.getInstance().get(Integer.valueOf(ticketId));
-/*     */     
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/* 120 */     int due = (int)POSUtil.getDouble(ticket.getDueAmount());
-/* 121 */     if (due != 0) {
-/* 122 */       POSMessageDialog.showError(Application.getPosWindow(), "Ticket is not fully paid");
-/* 123 */       return false;
-/*     */     }
-/*     */     
-/* 126 */     int option = JOptionPane.showOptionDialog(Application.getPosWindow(), "Ticket# " + ticket.getId() + " will be closed.", "Confirm", 2, 1, null, null, null);
-/*     */     
-/*     */ 
-/* 129 */     if (option != 0) {
-/* 130 */       return false;
-/*     */     }
-/*     */     
-/* 133 */     OrderController.closeOrder(ticket);
-/*     */     
-/* 135 */     return true;
-/*     */   }
-/*     */   
-/*     */   public void createCustomerMenu(JMenu menu)
-/*     */   {
-/* 140 */     menu.add(new CustomerExplorerAction());
-/*     */   }
-/*     */ }
+package com.oro.orderextension;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
-/* Location:              C:\Users\SOMYA\Desktop\divya\orocust-0.7.jar!\com\oro\orderextension\OrderWithCustomerService.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       0.7.1
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+import com.floreantpos.main.Application;
+import com.floreantpos.model.Customer;
+import com.floreantpos.model.OrderType;
+import com.floreantpos.model.Ticket;
+import com.floreantpos.model.User;
+import com.floreantpos.model.dao.TicketDAO;
+import com.floreantpos.model.dao.UserDAO;
+import com.floreantpos.ui.dialog.POSMessageDialog;
+import com.floreantpos.ui.views.order.DefaultOrderServiceExtension;
+import com.floreantpos.ui.views.order.OrderController;
+import com.floreantpos.ui.views.order.OrderView;
+import com.floreantpos.ui.views.order.RootView;
+import com.floreantpos.util.POSUtil;
+import com.floreantpos.util.TicketAlreadyExistsException;
+
+@PluginImplementation
+public class OrderWithCustomerService extends DefaultOrderServiceExtension {
+	public String getName() {
+		return "Order+Customer.";
+	}
+
+	public String getDescription() {
+		return "This extension enables storing customer information with ticekt";
+	}
+
+	public void init() {
+	}
+
+	public void createNewTicket(OrderType ticketType) throws TicketAlreadyExistsException {
+		DeliverySelectionDialog dialog = new DeliverySelectionDialog(Application.getPosWindow(), ticketType);
+		dialog.setSize(800, 650);
+		dialog.setLocationRelativeTo(Application.getPosWindow());
+		dialog.setVisible(true);
+
+		if (!dialog.isCanceled()) {
+			Customer customer = dialog.getCustomer();
+			Date deliveryDate = dialog.getDeliveryDate();
+			String shippingAddress = dialog.getDeliveryAddress();
+
+			Ticket ticket = new Ticket();
+			ticket.setPriceIncludesTax(Application.getInstance().isPriceIncludesTax());
+			ticket.setType(ticketType);
+			ticket.setTerminal(Application.getInstance().getTerminal());
+			ticket.setOwner(Application.getCurrentUser());
+			ticket.setShift(Application.getInstance().getCurrentShift());
+
+			ticket.setCustomer(customer);
+
+			if (dialog.willCustomerPickup()) {
+				ticket.setCustomerWillPickup(Boolean.valueOf(true));
+			} else {
+				ticket.setDeliveryDate(deliveryDate);
+				ticket.setDeliveryAddress(shippingAddress);
+				ticket.setExtraDeliveryInfo(dialog.getExtraDeliveryInfo());
+				ticket.setCustomerWillPickup(Boolean.valueOf(false));
+			}
+
+			Calendar currentTime = Calendar.getInstance();
+			ticket.setCreateDate(currentTime.getTime());
+			ticket.setCreationHour(Integer.valueOf(currentTime.get(11)));
+
+			OrderView.getInstance().setCurrentTicket(ticket);
+			RootView.getInstance().showView("ORDER_VIEW");
+		}
+	}
+
+	public void setCustomerToTicket(int ticketId) {
+	}
+
+	public void setDeliveryDate(int ticketId) {
+	}
+
+	public void assignDriver(int ticketId) {
+		List<User> drivers = UserDAO.getInstance().findDrivers();
+
+		if ((drivers == null) || (drivers.size() == 0)) {
+			POSMessageDialog.showError(Application.getPosWindow(), "No driver found to assign");
+			return;
+		}
+
+		Ticket ticket = TicketDAO.getInstance().get(Integer.valueOf(ticketId));
+
+		AssignDriverDialog dialog = new AssignDriverDialog(Application.getPosWindow());
+		dialog.setData(ticket, drivers);
+		dialog.setSize(550, 450);
+		dialog.setLocationRelativeTo(Application.getPosWindow());
+		dialog.setVisible(true);
+	}
+
+	public boolean finishOrder(int ticketId) {
+		Ticket ticket = TicketDAO.getInstance().get(Integer.valueOf(ticketId));
+
+		int due = (int) POSUtil.getDouble(ticket.getDueAmount());
+		if (due != 0) {
+			POSMessageDialog.showError(Application.getPosWindow(), "Ticket is not fully paid");
+			return false;
+		}
+
+		int option = JOptionPane.showOptionDialog(Application.getPosWindow(), "Ticket# " + ticket.getId() + " will be closed.", "Confirm", 2, 1, null, null, null);
+
+		if (option != 0) {
+			return false;
+		}
+
+		OrderController.closeOrder(ticket);
+
+		return true;
+	}
+
+	public void createCustomerMenu(JMenu menu) {
+		menu.add(new CustomerExplorerAction());
+	}
+}
+
+/*
+ * Location:
+ * C:\Users\SOMYA\Desktop\divya\orocust-0.7.jar!\com\oro\orderextension
+ * \OrderWithCustomerService.class Java compiler version: 5 (49.0) JD-Core
+ * Version: 0.7.1
  */
